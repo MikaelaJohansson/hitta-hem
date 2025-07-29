e9337dc83ca609aeaa6c9a070157f2efc05cfc30
Mikaela Johansson <mikaela.johansson87@gmail.com>
2025-07-29 12:37:47 +0200

    dist mapp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hitta-hem-backend.azurewebsites.net/api'
+  apiUrl: 'https://hitta-hem-backend-fdg3h7g0gpfpfpc9.swedencentral-01.azurewebsites.net/api'
 };
+
 
 
 
