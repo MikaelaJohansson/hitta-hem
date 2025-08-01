87a568d5658869977c29af7b391d19f499b22b2b
Mikaela Johansson <mikaela.johansson87@gmail.com>
2025-08-01 12:27:00 +0200

    fix

--- src/app/components/pages/adoption/adoption.component.ts
+++ src/app/components/pages/adoption/adoption.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { DogService } from '../../../services/dog.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-adoption',
   standalone: true,
-  imports: [NgFor, RouterLink],
+  imports: [NgFor, RouterLink,NgIf],
   templateUrl: './adoption.component.html',
   styleUrl: './adoption.component.scss'
 })
