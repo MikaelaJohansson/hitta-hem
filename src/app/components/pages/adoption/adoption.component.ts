058edcd1e62d65e694e851d967ac7b08dbd5d463
Mikaela Johansson <mikaela.johansson87@gmail.com>
2025-08-01 10:30:55 +0200

    fix img

--- src/app/components/pages/adoption/adoption.component.ts
+++ src/app/components/pages/adoption/adoption.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { DogService } from '../../../services/dog.service';
 import { NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-adoption',
   standalone: true,
-  imports: [NgFor,RouterLink],
+  imports: [NgFor, RouterLink],
   templateUrl: './adoption.component.html',
   styleUrl: './adoption.component.scss'
 })
 export class AdoptionComponent {
+  environment = environment;
+
   constructor(public dogService: DogService) {
     this.dogService.getDogsFromApi();
   }
 }
+
+
